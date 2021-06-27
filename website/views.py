72abd89c791cb1506d3419e123af322722135579
SarahKParks <sparks3721@outlook.com>
2021-06-26 23:30:57 -0500

    added note functionality

--- website/views.py
+++ website/views.py
@@ -1,14 +1,35 @@
 # store standard roots of website, everything that's not related to authentication
 
 # allows us to split up views for better organization
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 
 views = Blueprint('views', __name__)
 
-@views.route('/') # decorator
+@views.route('/', methods=['GET', 'POST']) # decorator
 @login_required
 def home():
+    if request.method == "POST":
+        note = request.form.get('note')
+        new_note = Note(data=note, user_id=current_user.id)
+        db.session.add(new_note)
+        db.session.commit()
+        flash('Note added!', category='success')
     return render_template("home.html", user=current_user) # reference current user to see if authenticated
 
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    note_id = note['note_id']
+    note = Note.query.get(note_id)
+    if note:
+        # if user owns this node
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
